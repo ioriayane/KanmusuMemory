3b48c3b6c1112245313be6cb8dbbdaf0ea03af7f
Akihiro Maeda <sola.1980.a@gmail.com>
2013-08-27 00:20:12 +0900

    add script to make deb package

--- scripts/deployubuntu.sh
+++ scripts/deployubuntu.sh
@@ -33,6 +33,20 @@ mv ${TMPPATH} ${APPNAME}
 #archive
 zip -ry ${APPNAME}-0.0-ubuntu-x86.zip ${APPNAME}
 
-
-
+#make deb package
+mkdir kanmusumemory
+cp -a scripts/deb/DEBIAN kanmusumemory/
+cp Readme.txt kanmusumemory/
+mkdir kanmusumemory/usr
+cp -a scripts/deb/share kanmusumemory/usr/
+mkdir -p kanmusumemory/usr/local/bin/kanmusumemory
+mkdir -p kanmusumemory/usr/local/lib/kanmusumemory
+cp -a ${APPNAME}/bin/* kanmusumemory/usr/local/bin/kanmusumemory/
+cp -a ${APPNAME}/lib/* kanmusumemory/usr/local/lib/kanmusumemory/
+cp -a scripts/deb/KanmusuMemory kanmusumemory/usr/local/bin/
+rm kanmusumemory/usr/local/bin/kanmusumemory/KanmusuMemory.sh
+cp -a scripts/deb/Desktop_KanmusuMemory.sh kanmusumemory/usr/local/bin/kanmusumemory/
+du -s kanmusumemory| cut -f 1| xargs echo Installed-Size: >> kanmusumemory/DEBIAN/control
+dpkg-deb -b kanmusumemory
+#rm -rf kanmusumemory
 
