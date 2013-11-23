5054fd81d6cc36dcfe74f7cafc7a4e1e4dc23f04
Takayuki ORITO <iori.ayane@gmail.com>
2013-11-23 15:46:47 +0900

    update version number

--- kanmusumemory_global.h
+++ kanmusumemory_global.h
@@ -19,8 +19,8 @@
 #define KANMEMO_NAME "KanmusuMemory"
 #define KANMEMO_NAME_FAV "KanmusuMemoryFav"
 #define KANMEMO_PROJECT "KanmemoProject"
-#define KANMEMO_VERSION "0.7"
-#define KANMEMO_VERSION_CODE 7
+#define KANMEMO_VERSION "0.8"
+#define KANMEMO_VERSION_CODE 8
 #define KANMEMO_DEVELOPERS (QStringList() \
     << "@IoriAYANE"\
     << "@task_jp"\
